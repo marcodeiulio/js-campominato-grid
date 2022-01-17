d20d9d644a599e59242e53c279732e4a4bc8bd05
marcodeiulio <mdeiulio@icloud.com>
2022-01-17 20:18:07 +0100

    - Added playToggle - Fixed #spotlight removal

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-/* 
-L'utente indica TRAMITE DOM un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
-con difficoltà 1 => tra 1 e 100
-con difficoltà 2 => tra 1 e 81
-con difficoltà 3 => tra 1 e 49
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
-*/
-
 //# Functions
 const createClassedCell = (cellClass) => {
 	const cell = document.createElement('div');
@@ -18,14 +10,14 @@ const toggleClassOnClick = (target, classToToggle) => {
 		target.classList.toggle(classToToggle);
 	});
 }
-/* 
+
 const toggleClass = (target, classToToggle) => {
 	if (target.classList.contains(classToToggle)) {
 		target.classList.remove(classToToggle);
-	} else if (!(target.classList.contains(classToToggle))) {
+	} else if (!target.classList.contains(classToToggle)) {
 		target.classList.add(classToToggle);
 	}
-} */
+}
 
 //# Program
 
@@ -57,38 +49,47 @@ const start = document.getElementById('start');
 
 const reset = document.getElementById('reset');
 
+let playToggle = false;
+
 start.addEventListener('click', function () {
 
 	const difficulty = parseInt(document.getElementById('difficulty').value);
 	console.log('Difficulty chosen: ' + difficulty);
+
+	if (playToggle) {
+		alert('The game will be reset.')
+		location.reload();
+	}
 
 	if (difficulty === 1) {
 		for (i = 0; i < totalOne; i++) {
 			const cell = createClassedCell(classOne);
 			grid.appendChild(cell);
 			cell.innerText = i + 1;
+			playToggle = true;
 			toggleClassOnClick(cell, classActive);
-			toggleClass(spotlight, classRemove);
 		}
 	} else if (difficulty === 2) {
 		for (i = 0; i < totalTwo; i++) {
 			const cell = createClassedCell(classTwo);
 			grid.appendChild(cell);
 			cell.innerText = i + 1;
+			playToggle = true;
 			toggleClassOnClick(cell, classActive);
-			toggleClass(spotlight, classRemove);
 		}
 	} else if (difficulty === 3) {
 		for (i = 0; i < totalThree; i++) {
 			const cell = createClassedCell(classThree);
 			grid.appendChild(cell);
 			cell.innerText = i + 1;
+			playToggle = true;
 			toggleClassOnClick(cell, classActive);
-			toggleClass(spotlight, classRemove);
 		}
 	} else {
 		alert('Please, choose a difficulty.');
 	}
+
+	toggleClass(spotlight, classRemove);
 });
 
 reset.addEventListener('click', function () {
