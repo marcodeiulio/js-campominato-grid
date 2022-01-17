31d0e425d9c713835214094cfc7418f66b890562
marcodeiulio <mdeiulio@icloud.com>
2022-01-17 17:07:24 +0100

    - Created difficulty 1 grid

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
+/* 
+L'utente indica TRAMITE DOM un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+*/
+
+
 console.log('duck');
+
+const cellsOne = 10;
+const cellsTwo = 9;
+const cellsThree = 7;
+
+const rowsOne = 10;
+const rowsTwo = 9;
+const rowsThree = 7;
+
+const totalOne = cellsOne * rowsOne;
+const totalTwo = cellsTwo * rowsTwo;
+const totalThree = cellsThree * rowsThree;
+
+const grid = document.getElementById('grid');
+
+for (i = 0; i < totalOne; i++) {
+	const cell = document.createElement('div');
+	cell.className = 'cell-one';
+	grid.appendChild(cell);
+	cell.innerText = i + 1;
+}
