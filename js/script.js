1068692033b4d04be42e90dd7b9342d1ad7ea356
marcodeiulio <mdeiulio@icloud.com>
2022-01-17 17:27:09 +0100

    - Created other cell classes - Created and implemented createClassedCell()

--- js/script.js
+++ js/script.js
@@ -6,9 +6,14 @@ con difficoltà 3 => tra 1 e 49
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
 
+//# Functions
+const createClassedCell = (cellClass) => {
+	const cell = document.createElement('div');
+	cell.className = cellClass;
+	return cell;
+}
 
-console.log('duck');
-
+//# Program
 const cellsOne = 10;
 const cellsTwo = 9;
 const cellsThree = 7;
@@ -21,11 +26,14 @@ const totalOne = cellsOne * rowsOne;
 const totalTwo = cellsTwo * rowsTwo;
 const totalThree = cellsThree * rowsThree;
 
+const classOne = 'cell-one';
+const classTwo = 'cell-Two';
+const classThree = 'cell-Three';
+
 const grid = document.getElementById('grid');
 
 for (i = 0; i < totalOne; i++) {
-	const cell = document.createElement('div');
-	cell.className = 'cell-one';
+	const cell = createClassedCell(classOne);
 	grid.appendChild(cell);
 	cell.innerText = i + 1;
 }
